893f0109e2f395a6c0612c1581ce79284fd81f08
ArenGamerZ <alex13gamerz@gmail.com>
2016-10-27 17:59:29 +0200

    Ver 2.4.1a rdy for testing

--- mbackup.sh
+++ mbackup.sh
@@ -4,7 +4,7 @@
 # Name: myBackup
 # Author: ArenGamerZ
 # Email: arendevel@gmail.com
-# Version: 2.3.0a
+# Version: 2.4.1a
 # Description: This is a Backup program that will help you to maintain, adminstrate and make your backup.
 ###########################################################################################################
 
@@ -12,7 +12,7 @@ BkPath=/mnt/Backup # This is the path where the backup will be stored
 BFolder=Backup # This is the name of the mountpoint where your Backup is stored
 DPath=/mnt/Data # This is the path where the data to be backuped will be taken from
 DFolder=Data # This is the name of the mountpoint where your Data is stored
-DtoBackup=('/mnt/Data/IT' '/mnt/Data/Music') # Put the folders you want to backup between '' and separated by spaces
+DtoBackup=('/mnt/Data/IT/' '/mnt/Data/Music/') # Put the folders you want to backup between '' and separated by spaces
 Device=/dev/sda1 # This is the device where the backup is stored
 
 function usage(){
@@ -28,36 +28,35 @@ function usage(){
 }
 
 function quit(){
-	if ls $BkPath; then umount -f $Device; fi
+	if df | grep -q $Device; then umount -f $Device; fi
 	exit 0
 }
 
 function backup(){
-	if ! ls $BkPath; then mount $Device; fi
+	if ! df | grep -q $Device; then mount $Device $BkPath; fi
 	for dir in ${DtoBackup[@]} 
 	do
 		cp -ruv --no-preserve=all $dir $BkPath
 	done
 	find $BkPath -type d -exec chmod -R 770 {} \;
 	find $BkPath -type f -exec chmod -R 660 {} \;
-	umount $Device
-	exit 0
+	umount -f $Device
 }
 
 function browse(){
 	clear
-	if ! ls $BkPath; then mount $Device; fi
-	nautilus $BkPath
+	if ! df | grep -q $Device; then mount $Device $BkPath; fi
+	nautilus $BkPath &
+	sleep 1
 	echo "Type enter when you finished..."
 	read pause
-	ps aux | grep nautilus | tr -s ' ' | cut -d' ' -f2 | while read i; do kill -9 $i; done
+	nautilus -q
 	umount -f $Device
-	exit 0
 }
 
 function recovery(){
 	clear
-	if ! ls $BkPath; then mount $Device; fi
+	if ! df | grep -q $Device; then mount $Device $BkPath; fi
 	if [ ! -d $DPath/recovered ]; then mkdir $DPath/recovered; fi
 	echo "Welcome to recovery tool"
 	echo
@@ -69,51 +68,63 @@ function recovery(){
 		if $file; then
 			echo "Is $file the file/folder you want to recover?(Y/n)"
 			read choice
-			if [ $choice == "\n" ] || [ $choice == "Y" ] || [ $choice == "y" ]; then cp -ri $file $DPath/recovered
+			if [ ! $choice ] || [ $choice == "Y" ] || [ $choice == "y" ]; then cp -ri $file $DPath/recovered
 			elif [ $choice == "N" ] || [ $choice == "n" ]; then continue
-			else echo "Assuming yes..." && continue 
+			else echo "Assuming yes..." && cp -ri $file $DPath/recovered
 			fi
-		echo -n "Continue recovering? (Y/n): "
-		read choice
-		if [ $choice == "\n" ] || [ $choice == "Y" ] || [ $choice == "y" ]; then continue
-		elif [ $choice == "N" ] || [ $choice == "n" ]; then break
-		else echo "Assuming yes..." && continue
+			echo -n "Continue recovering? (Y/n): "
+			read choice
+			if [ ! $choice ] || [ $choice == "Y" ] || [ $choice == "y" ]; then continue
+			elif [ $choice == "N" ] || [ $choice == "n" ]; then break
+			else echo "Assuming yes..." && continue
+			fi
+		else
+			echo "You didn't write anything!"
+			continue
 		fi
 	done
 }
 
 function clean(){
-	for bfile in `find $BkPath`
+	if ! df | grep -q $Device; then mount $Device $BkPath; fi
+	find "$BkPath" -mindepth 1 | while read bfile
 	do
-		dfile=$(echo $bfile | sed "s/$BFolder/$DFolder/")
-		if ! find $DPath -wholename $dfile; then rm -rf $bfile; fi
+	dfile=$(echo "$bfile" | sed "s/$BFolder/$DFolder/")
+	found=$(find "$DPath" -wholename "$dfile")
+	if [[ ! "$found" ]]; then
+		rm -rf "$bfile"
+	else
+		echo "Found file $dfile in $DPath, skipping deletion..."
+	fi
 	done
 }
 
 function menu(){
-	clear
-	echo "###################################################################"
-	echo
-	echo "1) Backup your files"
-	echo "2) Browse backup device to recover files"
-	echo "3) Recovery Tool"
-	echo "4) Clean older removed files"
-	echo "5) Exit"
-	echo
-	echo "###################################################################"
-	echo
-	echo
-	echo -n "Choose: "
-	read choice
+	while true; do
+		clear
+		echo "###################################################################"
+		echo
+		echo "1) Backup your files"
+		echo "2) Browse backup device to recover files"
+		echo "3) Recovery Tool"
+		echo "4) Clean older removed files"
+		echo "5) Exit"
+		echo
+		echo "###################################################################"
+		echo
+		echo
+		echo -n "Choose: "
+		read choice
 
-	case $choice in
-		1) backup ;;
-		2) browse ;;
-		3) recovery ;;
-		4) clean ;;
-		5) quit ;;
-		*) echo "Menu option is not correct, returning to menu" && menu ;;
-	esac
+		case $choice in
+			1) backup ;;
+			2) browse ;;
+			3) recovery ;;
+			4) clean ;;
+			5) quit ;;
+			*) echo "Menu option is not correct, returning to menu" && menu ;;
+		esac
+	done
 }
 
 if [[ $EUID -ne 0 ]]; then echo "GTFO! You need root privileges!"
