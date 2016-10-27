3722f75723d0a915a78f1cdd951cb99b45fdb0bf
ArenGamerZ <alex13gamerz@gmail.com>
2016-10-27 20:24:33 +0200

    Now it has a more friendly user interface

--- mbackup.sh
+++ mbackup.sh
@@ -6,6 +6,7 @@
 # Email: arendevel@gmail.com
 # Version: 2.5.0 stable
 # Description: This is a Backup program that will help you to maintain, adminstrate and make your backup.
+# Important: Set the vars below to suit your configuration, these are just an example.
 ###########################################################################################################
 
 BkPath=/mnt/Backup # This is the path where the backup will be stored
@@ -15,14 +16,24 @@ DFolder=Data # This is the name of the mountpoint where your Data is stored
 DtoBackup=('/mnt/Data/IT/' '/mnt/Data/Music/') # Put the folders you want to backup between '' and separated by spaces
 Device=/dev/sda1 # This is the device where the backup is stored
 
+#Colors
+red=`tput setaf 1`
+yellow=`tput setaf 3`
+blackback=`tput setab 0`
+reset=`tput sgr0`
+blue=`tput setaf 4`
+cyan=`tput setaf 6`
+bold=`tput bold`
+green=`tput setaf 2`
+
 function usage(){
 	echo """Usage: mbackup <OPTION>    
        OPTION:        
+		<noargs>	-->	Goes to main menu
 		--backup	--> 	Performs a backup and exits
 		--clean		--> 	Performs a clean of older removed files and exits
 		--browse	--> 	Browses the backup device with nautilus and exits
 		--recovery	-->	Goes directly to recovery tool
-		<noargs>	-->	Goes to main menu
 		-h --help	-->	Shows this help"""
 
 }
@@ -48,7 +59,7 @@ function browse(){
 	if ! df | grep -q $Device; then mount $Device $BkPath; fi
 	nautilus $BkPath &
 	sleep 1
-	echo "Type enter when you finished..."
+	echo "${bold}${green}Type enter when you finished...${reset}"
 	read pause
 	nautilus -q
 	umount -f $Device
@@ -58,28 +69,28 @@ function recovery(){
 	clear
 	if ! df | grep -q $Device; then mount $Device $BkPath; fi
 	if [ ! -d $DPath/recovered ]; then mkdir $DPath/recovered; fi
-	echo "Welcome to recovery tool"
+	echo "${blue}Welcome to recovery tool${reset}"
 	echo
 	while true
 	do
-	    echo -n "Which folder/file do you want to recover: "
+	    echo -n "${bold}${green}Which folder/file do you want to recover: ${reset}"
 		read name
 		file=$(find $BkPath -name $name)
 		if $file; then
-			echo "Is $file the file/folder you want to recover?(Y/n)"
+			echo "${bold}${cyan}Is $file the file/folder you want to recover?(Y/n) ${reset}"
 			read choice
 			if [ ! $choice ] || [ $choice == "Y" ] || [ $choice == "y" ]; then cp -ri $file $DPath/recovered
 			elif [ $choice == "N" ] || [ $choice == "n" ]; then continue
-			else echo "Assuming yes..."; cp -ri $file $DPath/recovered
+			else echo "${bold}${yellow}Assuming yes...${reset}"; cp -ri $file $DPath/recovered
 			fi
-			echo -n "Continue recovering? (Y/n): "
+			echo -n "${bold}${green}Continue recovering? (Y/n): ${reset}"
 			read choice
 			if [ ! $choice ] || [ $choice == "Y" ] || [ $choice == "y" ]; then continue
 			elif [ $choice == "N" ] || [ $choice == "n" ]; then break
-			else echo "Assuming yes..."; continue
+			else echo "${bold}${yellow}Assuming yes...${reset}"; continue
 			fi
 		else
-			echo "You didn't write anything!"
+			echo "${red}You didn't write anything!${reset}"
 			continue
 		fi
 	done
@@ -92,7 +103,7 @@ function clean(){
 	do
 	dfile=$(echo "$bfile" | sed "s/$BFolder/$DFolder/")
 	if [[ ! -e "$dfile" ]]; then
-		echo "File $dfile not found, deleting..."
+		echo "${red}File $dfile not found, deleting from backup...${reset}"
 		rm -rf "$bfile"
 	else
 		continue
@@ -105,18 +116,20 @@ function menu(){
 	while true
 	do
 		clear
-		echo "###################################################################"
+		echo "${bold}${green}###################################################################${reset}"
+		echo
+		echo "${bold}${blue}			WELCOME TO MYBACKUP MENU${cyan}"
 		echo
 		echo "1) Backup your files"
 		echo "2) Browse backup device to recover files"
-		echo "3) Recovery Tool"
+		echo "3) Recovery Tool"                                      
 		echo "4) Clean older removed files"
 		echo "5) Exit"
 		echo
-		echo "###################################################################"
+		echo "${bold}${green}###################################################################${reset}"
 		echo
 		echo
-		echo -n "Choose: "
+		echo -n "${bold}${yellow}Choose: ${reset}"
 		read choice
 
 		case $choice in
@@ -125,12 +138,12 @@ function menu(){
 			3) recovery ;;
 			4) clean ;;
 			5) quit ;;
-			*) echo "Menu option is not correct, returning to menu" && menu ;;
+			*) echo -ne "\n${red}Menu option is not correct, returning to menu${reset}";  sleep 2; continue ;;
 		esac
 	done
 }
 
-if [[ $EUID -ne 0 ]]; then echo "GTFO! You need root privileges!"
+if [[ $EUID -ne 0 ]]; then echo "${red}GTFO! You need root privileges!${reset}"
 else
 	if [[ $# -eq 0 ]]; then menu
 	elif [[ $# -eq 1 ]]; then
@@ -139,10 +152,11 @@ else
 			--browse) browse ;;
 			--recovery) recovery ;;
 			--clean) clean ;;
-			*) echo "Parameter error!" && echo && usage ;;
+			-h|--help) usage ;;
+			*) echo "${red}Parameter error!${reset}"; echo; usage ;;
 		esac
 	else
-		echo "Parameter error!"
+		echo "${red}Parameter error!${reset}"
 		usage
 	fi
 fi
